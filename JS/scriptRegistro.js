771aad8f047013bbb7c55f38f1418d9d5882ce7b
xtream2000c <carlmarcast@gmail.com>
2021-11-28 15:34:41 +0100

    Empezando script registro

---
+++ JS/scriptRegistro.js
@@ -0,0 +1,10 @@
+document.getElementById("formulario").addEventListener("submit", registro, false);
+
+function registro(){
+
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById ("email").value;
+    var usuario = document.getElementById ("usuario").value;
+    var contrasena = document.getElementById ("contrasena").value;
+
+}
