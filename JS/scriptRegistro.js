f53e5e5009bd39a1f6c2d391edb97dea814bf479
xtream2000c <carlmarcast@gmail.com>
2021-11-28 21:45:11 +0100

    Ya puede registrarse un usuario

--- JS/scriptRegistro.js
+++ JS/scriptRegistro.js
@@ -1,10 +1,27 @@
-document.getElementById("formulario").addEventListener("submit", registro, false);
+window.onload = load;
 
-function registro(){
+function load(){
+    document.getElementById("formulario").addEventListener("submit", registro, false);
+}
 
+
+function registro(event){
+    event.preventDefault();
     var nombre = document.getElementById("nombre").value;
     var email = document.getElementById ("email").value;
     var usuario = document.getElementById ("usuario").value;
     var contrasena = document.getElementById ("contrasena").value;
 
+    var registrojson = {'usuario' : usuario, 'nombre' : nombre, 'email' : email, 'contrasena' : contrasena};
+
+    
+
+    var confirmacion = confirm("¿Desea confirmar el registro?");
+
+    if (confirmacion) {
+        localStorage.setItem('usuarioRegistrado', JSON.stringify(registrojson));
+        location.href="login.html"
+    }else{
+        event.preventDefault();
+    }
 }
